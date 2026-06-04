306560ba9d511c2e7ae24c8fd2214e7dbdc35288
Antony <tony@tony>
2026-06-04 16:20:39 +0200

    changed ai support pop up

--- app/javascript/dashboard/components/widgets/AIAssistanceButton.vue
+++ app/javascript/dashboard/components/widgets/AIAssistanceButton.vue
@@ -1,95 +1,109 @@
-<script>
-import { ref } from 'vue';
+<script setup>
+import { ref, computed, onMounted, onUnmounted } from 'vue';
 import { useAI } from 'dashboard/composables/useAI';
 import { useExtraAIAssist } from 'dashboard/composables/useExtraAIAssist';
 import { useKeyboardEvents } from 'dashboard/composables/useKeyboardEvents';
-import AIAssistanceModal from './AIAssistanceModal.vue';
+import { useMapGetter } from 'dashboard/composables/store';
+import { REPLY_EDITOR_MODES } from './WootWriter/constants';
 import { CMD_AI_ASSIST } from 'dashboard/helper/commandbar/events';
 import { emitter } from 'shared/helpers/mitt';
+import AIAssistanceModal from './AIAssistanceModal.vue';
 import NextButton from 'dashboard/components-next/button/Button.vue';
 
-export default {
-  components: {
-    NextButton,
-    AIAssistanceModal,
+const props = defineProps({
+  message: {
+    type: String,
+    default: '',
   },
-  emits: ['replaceText'],
-  setup(props, { emit }) {
-    const { isAIIntegrationEnabled, draftMessage, recordAnalytics } = useAI();
-    const { isExtraAIEnabled } = useExtraAIAssist();
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-    const initialMessage = ref('');
+const emit = defineEmits(['replaceText']);
 
-    const initializeMessage = draftMsg => {
-      initialMessage.value = draftMsg;
-    };
-    const keyboardEvents = {
-      '$mod+KeyZ': {
-        action: () => {
-          if (initialMessage.value) {
-            emit('replaceText', initialMessage.value);
-            initialMessage.value = '';
-          }
-        },
-        allowOnFocusedInput: true,
-      },
-    };
-    useKeyboardEvents(keyboardEvents);
+const { isAIIntegrationEnabled, draftMessage, recordAnalytics } = useAI();
+const { isExtraAIEnabled } = useExtraAIAssist();
+const replyMode = useMapGetter('draftMessages/getReplyEditorMode');
 
-    return {
-      initialMessage,
-      initializeMessage,
-      recordAnalytics,
-      isAIIntegrationEnabled,
-      isExtraAIEnabled,
-      draftMessage,
-    };
-  },
-  data: () => ({
-    showAIAssistanceModal: false,
-    aiOption: '',
-  }),
-  computed: {
-    isAIAssistEnabled() {
-      return this.isExtraAIEnabled || this.isAIIntegrationEnabled;
+const showAIAssistanceModal = ref(false);
+const aiOption = ref('');
+const initialMessage = ref('');
+
+const isAIAssistEnabled = computed(
+  () => isExtraAIEnabled.value || isAIIntegrationEnabled.value
+);
+
+const isTranslateMode = computed(
+  () => replyMode.value === REPLY_EDITOR_MODES.TRANSLATE
+);
+
+const hasTypedText = computed(() => Boolean(props.message?.trim()));
+
+const showAIAssist = computed(
+  () => isAIAssistEnabled.value && !isTranslateMode.value
+);
+
+const onAIAssist = option => {
+  aiOption.value = option;
+  showAIAssistanceModal.value = true;
+};
+
+const openAIAssist = () => {
+  initialMessage.value = draftMessage.value;
+  const ninja = document.querySelector('ninja-keys');
+  ninja.classList.add('ai-assist-popup');
+  const onClosed = () => {
+    ninja.classList.remove('ai-assist-popup');
+    ninja.removeEventListener('closed', onClosed);
+  };
+  ninja.addEventListener('closed', onClosed);
+  ninja.open({ parent: 'ai_assist' });
+};
+
+const hideAIAssistanceModal = () => {
+  recordAnalytics('DISMISS_AI_SUGGESTION', { aiOption: aiOption.value });
+  showAIAssistanceModal.value = false;
+};
+
+const insertText = message => {
+  emit('replaceText', message);
+};
+
+const keyboardEvents = {
+  '$mod+KeyZ': {
+    action: () => {
+      if (initialMessage.value) {
+        emit('replaceText', initialMessage.value);
+        initialMessage.value = '';
+      }
     },
-  },
-  mounted() {
-    emitter.on(CMD_AI_ASSIST, this.onAIAssist);
-    this.initializeMessage(this.draftMessage);
-  },
-  methods: {
-    hideAIAssistanceModal() {
-      this.recordAnalytics('DISMISS_AI_SUGGESTION', {
-        aiOption: this.aiOption,
-      });
-      this.showAIAssistanceModal = false;
-    },
-    openAIAssist() {
-      this.initializeMessage(this.draftMessage);
-      const ninja = document.querySelector('ninja-keys');
-      ninja.open({ parent: 'ai_assist' });
-    },
-    onAIAssist(option) {
-      this.aiOption = option;
-      this.showAIAssistanceModal = true;
-    },
-    insertText(message) {
-      this.$emit('replaceText', message);
-    },
+    allowOnFocusedInput: true,
   },
 };
+useKeyboardEvents(keyboardEvents);
+
+onMounted(() => {
+  emitter.on(CMD_AI_ASSIST, onAIAssist);
+  initialMessage.value = draftMessage.value;
+});
+
+onUnmounted(() => {
+  emitter.off(CMD_AI_ASSIST, onAIAssist);
+});
 </script>
 
 <template>
   <div>
-    <div v-if="isAIAssistEnabled" class="relative">
+    <div v-if="showAIAssist" class="relative">
       <NextButton
         v-tooltip.top-end="$t('INTEGRATION_SETTINGS.OPEN_AI.AI_ASSIST')"
         icon="i-ph-magic-wand"
         slate
         faded
         sm
+        :disabled="disabled || !hasTypedText"
         @click="openAIAssist"
       />
       <woot-modal
